ec0d5b427f5f0b32b9fd753a4de4cc495002eda8
Brian Norton <brian.nort@gmail.com>
2015-06-01 11:51:48 -0700

    Add channel.js example model.

---
+++ examples/models/channel.js
@@ -0,0 +1,20 @@
+Channel = Document.progeny('Channel', {
+
+}, {
+  classMethods: {
+    attrs: {
+      String: { name: 'n', slug: 's', token: 't' },
+      Integer: { buffered: 'bu', capped: 'c'}
+    },
+    allow: ['name', 'slug', 'token', 'buffered'],
+    validate: {
+      presence: ['name'],
+      format: { slug: /^#\w+/ }
+    },
+    beforeCreate: [
+      function() { this.set('token', 'C-'+(Math.random()*10000000)) }
+    ]
+  }
+});
+
+module.exports = Channel;
