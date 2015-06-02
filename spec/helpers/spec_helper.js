243934e6bbb3649863ba27ce79e817002c165667
Brian Norton <brian.nort@gmail.com>
2015-06-01 17:24:45 -0700

    Make sure the default adapter is cleared before every test run.

--- spec/helpers/spec_helper.js
+++ spec/helpers/spec_helper.js
@@ -1,2 +1,13 @@
-beforeEach(function () {
-});
+(function(once) { if(once) return function() {};
+  global.itBehavesLikeAnAdapter = require('./adapter_helper');
+  global._specHelper = true;
+
+  beforeEach(function() {
+  });
+
+  afterEach(function() {
+    Document.Adapter.clear();
+  });
+
+  module.exports = global._specHelper
+})(global._specHelper);
