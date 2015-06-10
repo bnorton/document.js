f1582e62b1a515a1f20823807e5f4f12e2e95ca0
Brian Norton <brian.nort@gmail.com>
2015-06-10 12:47:34 -0700

    Put the library in local mode in the browser.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*!
+ * document.js (c) 2015 Brian Norton
+ * This library may be freely distributed under the MIT license.
+ */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.model = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 require('progenitor.js')();
 
@@ -3965,7 +3969,7 @@ require('progenitor.js')();
 
 var extend = require('extend'),
   inflect = require('i')(),
-  localMode = typeof window === 'undefined' || process.env.NODE_ENV === 'test',
+  localMode = typeof window !== 'undefined' || !process.env.NODE_ENV || process.env.NODE_ENV === 'test',
   Adapter = localMode ? require('./memory_adapter') : require('./mongo_adapter'),
   noop = function() { };
 
